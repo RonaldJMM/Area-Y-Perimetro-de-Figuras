9f186f06474687f438e8e282ae8c8c52dc75f6d2
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-15 14:28:04 -0500

    Validacion y Operaciones de las Figuras.
    Se hace la respectiva validacion de las figuras con cada uno de los datos asignados previamente.

--- Main.java
+++ Main.java
@@ -8,13 +8,34 @@ public class Main{
 		System.out.println("");
 		System.out.println("");
 
-		//variables y numeros a evaluar
-		double ladoCuadrado = 30;
-		double alturaTriangulo = 25.2;
-		double baseTriangulo = 13.2;
+		//variables y numeros a evaluar en centimetros
+		double ladosCuadrado = 30.3;
+		double lado1Triangulo = 15.0;
+		double lado2Triangulo = 24.0;
+		double lado3Triangulo = 15.0;
+		double alturaTriangulo = 0.0;
+		double baseTriangulo = 24.0;
 		double radioCirculo = 67.3 ;
+		//resultados
+		double totalAreaCuadrado,totalPerimetroCuadrado;
+		double totalAreaTriangulo,totalPerimetroTriangulo;
+		double totalAreaCirculo,totalPerimetroCirculo;
 
-		
+
+		//validacion del cuadrado
+		totalAreaCuadrado = ladosCuadrado*ladosCuadrado;
+		totalPerimetroCuadrado = ladosCuadrado*4;
+
+		//validacion del triangulo
+		alturaTriangulo=Math.sqrt((Math.pow(lado1Triangulo,2))-(Math.pow((baseTriangulo/2),2)));
+		totalAreaTriangulo=baseTriangulo*alturaTriangulo;
+		totalPerimetroTriangulo=lado1Triangulo+lado2Triangulo+lado3Triangulo;
+
+		//validacion del circulo
+		totalPerimetroCirculo=2*3.1416*radioCirculo;
+		totalAreaCirculo=3.1416*(Math.pow(radioCirculo,2));
+
+
 	}
 
 }
