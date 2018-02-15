025809d515fbbdbf9cfb3f6dd71962cad30473ae
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-15 14:05:58 -0500

    Variables y Numeros de Figuras.
    Declaracion de variables para las figuras, Triangulo, Cuadrado y Circulo.

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+	
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("Area Y Perimetro de Figuras");
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio No.2");
+		System.out.println("");
+		System.out.println("");
+
+		//variables y numeros a evaluar
+		double ladoCuadrado = 30;
+		double alturaTriangulo = 25.2;
+		double baseTriangulo = 13.2;
+		double radioCirculo = 67.3 ;
+
+		
+	}
+
+}
